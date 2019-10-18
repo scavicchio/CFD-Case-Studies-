d778a6e97b26dfacd32c63572a3e49e97268b5bd
Salvy Cavicchio <15899724+scavicchio@users.noreply.github.com>
2019-10-18 14:52:32 -0400

    Create testrun.bat

---
+++ testrun.bat
@@ -0,0 +1,14 @@
+REM this batch file will run all the cases
+REM simultaneously using just a few threads on each
+REM this is actually faster than runnning them 
+REM individually with all the threads! But only 
+REM possible if you have the RAM for it!
+
+REM will need to solve a total of 4 solutions
+REM this computer has 16 threads so I can put 4 for each
+REM but instead I will put 3 for one and the rest 4
+REM so that I can save one thread for windows processes and
+REM avoid a crash! 
+echo 
+
+start "test" fds_local -o 1 CFD-03-10.fds
