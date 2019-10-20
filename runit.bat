2a0e590c0859ef039ab88731ff1560a070a528e7
Salvy Cavicchio <15899724+scavicchio@users.noreply.github.com>
2019-10-20 17:08:05 -0400

    result first run

--- runit.bat
+++ runit.bat
@@ -6,15 +6,13 @@ REM possible if you have the RAM for it!
 
 REM will need to solve a total of 4 solutions
 REM this computer has 16 threads so I can put 4 for each
-REM but instead I will put 3 for one and the rest 4
-REM so that I can save one thread for windows processes and
-REM avoid a crash! 
+
 echo 
 call fdsinit.bat
-start "Case 1" /d "case1" fds_local -p 2 -o 3 CFD-03-10-1.fds 
+start "Case 1" /d "case1" fds_local -p 2 -o 2 CFD-03-10-1.fds 
 
-start "Case 2" /d "case2" fds_local -p 2 -o 4 CFD-03-10-2.fds 
+start "Case 2" /d "case2" fds_local -p 2 -o 1 CFD-03-10-2.fds 
 
-start "Case 3" /d "case3" fds_local -p 2 -o 4 CFD-03-10-3.fds 
+start "Case 3" /d "case3" fds_local -p 2 -o 2 CFD-03-10-3.fds 
 
-start "Case 4" /d "case4" fds_local -p 2 -o 4 CFD-03-10-4.fds 
+start "Case 4" /d "case4" fds_local -p 2 -o 3 CFD-03-10-4.fds 
